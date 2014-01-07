a695d994ec6e96cdc4fe7cbbab5a849c0a1eec4d
Laure Fischer <laurefischer@Laures-MacBook-Air.local>
2014-01-07 16:50:29 -0500

    created calculator menu and prompt for user input

--- calc.rb
+++ calc.rb
@@ -1,1 +1,9 @@
 # Your code goes here
+def calculator_menu
+	puts "Enter one of the following calculations you would like to perform:
+	addition
+	subtraction
+	multiplication
+	division"
+	gets.chomp
+end
