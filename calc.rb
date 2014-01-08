97c78903d5b05041d6d72b33e13c8d4367f95b6e
Laure Fischer <laurefischer@Laures-MacBook-Air.local>
2014-01-07 21:50:51 -0500

    added square and cube methods

--- calc.rb
+++ calc.rb
@@ -1,4 +1,4 @@
-# Present the user with a menu of operations to choose from, and ask them to enter their selection
+# Define the methods for the calculations
 def addition (num1, num2)
 	num1 + num2
 end
@@ -15,11 +15,22 @@ def division (num1, num2)
 	num1 / num2
 end
 
+def square (num1)
+	num1 * num1
+end
+
+def cube (num1)
+	num1 * num1 * num1
+end
+
+# Prompt the user to select a calculation from a menu
 puts "Enter one of the following calculations you would like to perform:
 addition
 subtraction
 multiplication
-division"
+division
+square
+cube"
 
 user_choice = gets.chomp
 # After the user selects an operation, prompt them to enter the first number for the calculation
@@ -32,16 +43,29 @@ case user_choice
 		puts "Please enter the first number you'd like to multiply"
 	when "division"
 		puts "Please enter the first number for division"
+	when "square"
+		puts "Please enter the number you would like to square"
+	when "cube"
+		puts "Please enter the number you would like to cube"
 	else
-		puts "Please choose from one of the four options provided"
+		puts "Please choose from one of the calculation options provided"
 end
 
 first_number = gets.chomp.to_i
-
-puts "Please enter the second number for the operation"
+# If the user chooses addition, subtraction, multiplication or division, prompt them to enter a second number
+case user_choice
+  when "addition", "subtraction", "multiplication", "division"
+    puts "Please enter the second number for the operation"
+  when "square"
+  	puts square(first_number)
+  when "cube"
+  	puts cube(first_number)
+  else
+  	puts "Please choose from one of the calculation options provided"
+ end
 
 second_number = gets.chomp.to_i
-
+# Calculate the answer for addition, subtraction, multiplication or division
 case user_choice
   when "addition"
     puts addition(first_number, second_number)
